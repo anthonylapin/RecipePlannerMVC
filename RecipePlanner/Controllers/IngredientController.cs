eb1625774b30265b0ad4caf756d1fea18c4881ce
a2.lapin <a2.lapin@itransition.com>
2021-04-18 23:43:18 +0300

    Delete & Update recipes added

---
+++ RecipePlanner/Controllers/IngredientController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RecipePlanner.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RecipePlanner.Controllers
+{
+    public class IngredientController : Controller
+    {
+        public IActionResult Create(Recipe recipe, string name, decimal quantity, string measurementValue)
+        {
+            var ingredient = new Ingredient() { Name = name, Quantity = quantity, MeasurementValue = measurementValue };
+            recipe.Ingredients.Add(ingredient);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
--- RecipePlanner/Controllers/RecipeController.cs
+++ RecipePlanner/Controllers/RecipeController.cs
@@ -24,6 +24,66 @@ namespace RecipePlanner.Controllers
             return View(_context.Recipes);
         }
 
+        public IActionResult Create()
+        {
+            return View("CreateEdit", new Recipe());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Recipe recipe)
+        {
+            recipe.Id = Guid.NewGuid();
+            _context.Recipes.Add(recipe);
+            await _context.SaveChanges();
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Update(Guid id)
+        {
+            var recipe = _context.Recipes.Find(r => r.Id == id);
+            
+            if (recipe == null)
+            {
+                return NotFound();
+            }
+
+            return View("CreateEdit", recipe);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Update(Recipe recipe)
+        {
+            var existingRecipe = _context.Recipes.Find(r => r.Id == recipe.Id);
+
+            if (existingRecipe == null)
+            {
+                return BadRequest();
+            }
+
+            existingRecipe.Name = recipe.Name;
+            existingRecipe.Instruction = recipe.Instruction;
+
+            await _context.SaveChanges();
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            var recipe = _context.Recipes.FirstOrDefault(r => r.Id == id);
+
+            if (recipe == null)
+            {
+                return NotFound();
+            }
+
+            _context.Recipes.Remove(recipe);
+            await _context.SaveChanges();
+
+            return RedirectToAction(nameof(Index));
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
